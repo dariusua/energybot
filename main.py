ee3b839cff5f5c0dde11867ee644a27569e2ec9b
Yurii <demaniik@gmail.com>
2022-12-04 20:43:13 +0200

    all working

--- main.py
+++ main.py
@@ -64,6 +64,16 @@ def sendforall(message):
         text = message.text[12:]
         for result in results:
             bot.send_message(result[0], {text})
+        cursor.execute("SELECT id FROM group2")
+        results = cursor.fetchall()
+        text = message.text[12:]
+        for result in results:
+            bot.send_message(result[0], {text})
+        cursor.execute("SELECT id FROM group3")
+        results = cursor.fetchall()
+        text = message.text[12:]
+        for result in results:
+            bot.send_message(result[0], {text})
         connect.commit()
     else:
         bot.send_message(message.from_user.id, "Для виконання цієї команди ви повинні бути адміном бота.")
