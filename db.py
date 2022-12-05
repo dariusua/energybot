c1e1a81ba2c4ad30f4d733fd62624016602156b4
Yurii <demaniik@gmail.com>
2022-12-05 19:08:26 +0200

    values to = in def add_user_g1

--- db.py
+++ db.py
@@ -7,7 +7,7 @@ class Database:
 
     def add_user_g1(self, user_id):
         with self.connection:
-            data = self.cursor.execute("SELECT user_id FROM `group1` WHERE `user_id` VALUES (?)", (user_id,)).fetchone()
+            data = self.cursor.execute("SELECT `user_id` FROM `group1` WHERE `user_id` = ?", (user_id,)).fetchone()
             if data is None:
                 self.cursor.execute("INSERT INTO `group1` (`user_id`, `active`) VALUES (?, ?)", (user_id, "1",))
             self.cursor.execute("DELETE FROM `group2` WHERE `id` VALUES (?)", (user_id,))
