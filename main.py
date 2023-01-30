86201c49d2f2e758cd6031d025ca746e7d9a4313
dariusua <demaniik@gmail.com>
2023-01-30 23:32:48 +0200

    stop bot from crashing v1.5.8

--- main.py
+++ main.py
@@ -80,10 +80,9 @@ def sendforall(message: types.Message):
             active_value = row[0]
             set_active = cursor.execute(f"SELECT active FROM database WHERE user_id = {active_value}")
             try:
-                msg = bot.send_message(row[0], text)
+                bot.send_message(row[0], text)
                 if set_active != 1:
                     cursor.execute(f"UPDATE database SET active = 1 WHERE user_id = {active_value}")
-                bot.delete_message(msg.chat.id, msg.message_id)
             except:
                 cursor.execute(f"UPDATE database SET active = 0 WHERE user_id = {active_value}")
                 time.sleep(1)
